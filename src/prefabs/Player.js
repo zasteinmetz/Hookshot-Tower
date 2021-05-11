aece45c318e883b26860a373e893976bd8156eaf
Zackary <zackarydsteinmetz@gmail.com>
2021-05-10 17:29:44 -0700

    Added basic player prefab (not much there)

---
+++ src/prefabs/Player.js
@@ -0,0 +1,8 @@
+class Player extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+
+        // add object to the existing scene
+        scene.add.existing(this); 
+    }
+}

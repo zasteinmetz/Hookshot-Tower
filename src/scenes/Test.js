a11d6d7a607c33fd0d82bc5e7c290c6c1beb9922
Zackary <zackarydsteinmetz@gmail.com>
2021-05-12 15:49:13 -0700

    Fixed gravity Errors

--- src/scenes/Test.js
+++ src/scenes/Test.js
@@ -5,12 +5,12 @@ class Test extends Phaser.Scene {
 
     create(){
         this.MAX_VELOCITY = 300;    //maximum velocity in pixils per second
-        this.physics.gravity.y = 1000;
+        this.physics.world.gravity.y = 1000;
 
         // make immovable ground out of tiles
         this.ground = this.add.group();
         for(let i = 0; i < game.config.width; i += tileSize){
-            let groundTile = this.physics.add.sprite(i, game.config.height - tileSize, 'placeholder').setScale(SCALE).setOrigin(0);
+            let groundTile = this.physics.add.sprite(i, game.config.height - tileSize, 'placeholder').setScale(0.5).setOrigin(0);
             groundTile.body.immovable = true;
             groundTile.body.gravity = false;
             this.ground.add(groundTile);

0e9954d5922ac64cb04cbe2853dd1d808ec7c8fd
Zackary <zackarydsteinmetz@gmail.com>
2021-05-20 17:19:37 -0700

    Added bat Prefab

---
+++ src/prefabs/Bat.js
@@ -0,0 +1,26 @@
+class Bat extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+        // add object to the existing scene
+        scene.add.existing(this); 
+        scene.physics.add.existing(this);
+        this.body.allowGravity = false;
+        this.speed = 200.0;
+        this.moveingLeft = true;
+        this.setVelocityX(-this.speed);
+    }
+    update() {
+    }
+    switchMovement(){
+        if(this.moveingLeft == true){
+            this.moveingLeft = false;
+            console.log('switch right');
+            this.setVelocityX(this.speed);
+        }
+        else if(this.moveingLeft == false){
+            this.moveingLeft = true;
+            console.log('switch left');
+            this.setVelocityX(-this.speed);
+        }
+    }
+}

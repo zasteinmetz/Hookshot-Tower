d182aa9ed8e72d5024c954d355cb520280bf3b0b
zasteinmetz <zackarydsteinmetz@gmail.com>
2021-12-17 17:19:05 -0800

    Used Teacher's Suggestion to modify hint text

--- src/scenes/Level1.js
+++ src/scenes/Level1.js
@@ -36,7 +36,7 @@ class Level1 extends Phaser.Scene {
 
         //Add explanatory text to teach mechanics
         this.firstNote = this.add.text( (6 * 32), (game.config.height * 2) - (4 * 32), 'WASD to move', "28px").setOrigin(0.5);
-        this.secondNote = this.add.text( (10 * 32), (game.config.height * 2) - (9 * 32), 'Click to grapple', "28px").setOrigin(0.5);
+        this.secondNote = this.add.text( (10 * 32), (game.config.height * 2) - (9 * 32), 'Click RMB (Right Mouse Button) on a platform to grapple', "28px").setOrigin(0.5);
 
         this.player = new Player(this, 180, game.config.height * 2 - 64, 200.0, 'player').setOrigin(0);
         this.player.collides = true;
